0da45187d3049610655bba7ad2b9da8286884969
lobintsev <107392652+lobintsev@users.noreply.github.com>
2023-03-14 19:41:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
             fontWeight='extrabold' fontFamily="heading" size='3xl'>
         Ready for every project</Heading>
 
- <ButtonGroup spacing='2' display={['none', 'none', 'flex', 'flex']}>
+ <ButtonGroup spacing='2' display='flex'>
           
           
   <Button variant="outline" variantColor="teal">Real Estate</Button>
