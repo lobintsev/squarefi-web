b398c2f57e5026201ceb2a3abba6e8df2c80d522
lobintsev <a.lobintsev@sky-logic.ru>
2023-04-01 13:21:45 +0300

    ddd

--- src/theme.js
+++ src/theme.js
@@ -5,13 +5,15 @@ const theme = extendTheme({
     styles: {
         global: {
           'html, body': {
-              bg: 'gray.800'
+              _dark: { bg: 'gray.800' }
           }}},
           fonts: {
             body: "LoosWide, sans-serif",
             heading: "LoosWide, sans-serif",
             mono: "Menlo, monospace",
-          }
+          },
+          initialColorMode: 'dark',
+          useSystemColorMode: false,
 });
 
 export default theme;
